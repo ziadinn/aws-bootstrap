58574a6ea050c4d488a1db346de0efd97a41bbfe
Ziadin Navarro <ziadinnavarro4@gmail.com>
2023-12-30 05:33:18 -0800

    Create infrastructure via CloudFormation

---
+++ deploy-infra.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+STACK_NAME=awsbootstrap
+REGION=us-east-1
+CLI_PROFILE=awsbootstrap
+
+EC2_INSTANCE_TYPE=t2.micro
+
+echo -e "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+    --region $REGION \
+    --profile $CLI_PROFILE \
+    --stack-name $STACK_NAME \
+    --template-file main.yml \
+    --no-fail-on-empty-changeset \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides \
+      EC2InstanceType=$EC2_INSTANCE_TYPE
+
+if [ $? -eq 0 ]; then
+    aws cloudformation list-exports \
+        --profile awsbootstrap \
+        --query "Exports[?Name=='InstanceEndpoint'].Value"
+fi
